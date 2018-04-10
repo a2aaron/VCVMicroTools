301f6227dc2e4636030163148d5680656a8ee426
Aaron Kofsky <a2aarontothe2@gmail.com>
2018-04-09 21:38:29 -0400

    Make placement of Widgets more compact. Also, add useless knobs.
    The knobs do nothing at the moment, but will soon.
    The creation of the PushButton widgets has been simplified by
    not copying-pasting them 8 times.

--- src/PushButton.cpp
+++ src/PushButton.cpp
@@ -5,7 +5,9 @@
 
 struct PushButton : Module {
     enum ParamIds {
-        NUM_PARAMS = NUM_CHANNELS
+        LIGHT_PARAM = 0,
+        KNOB_PARAM = NUM_CHANNELS,
+        NUM_PARAMS = LIGHT_PARAM + KNOB_PARAM,
     };
     enum OutputIds {
         NUM_OUTPUTS = NUM_CHANNELS
@@ -22,7 +24,8 @@ struct PushButton : Module {
 };
 
 void PushButton::step() {
-    for (int i = 0; i < NUM_PARAMS; i++) {
+    // printf("%s\n", LIGHT_PARAM);
+    for (int i = 0; i < NUM_CHANNELS; i++) {
         float out = 0.0f;
         state[i] = params[i].value > 0 ? true : false;
         if (state[i]) {
@@ -52,39 +55,14 @@ PushButtonWidget::PushButtonWidget(PushButton *module) : ModuleWidget(module) {
     addChild(Widget::create<ScrewSilver>(Vec(15, 365)));
     addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 30, 365)));
 
-    // Handles on click stuff
-    addParam(ParamWidget::create<LEDBezel>(mm2px(Vec(16.57, 18.165)), module, 0, 0.0f, 1.0f, 0.0f));
-    addParam(ParamWidget::create<LEDBezel>(mm2px(Vec(16.57, 28.164)), module, 1, 0.0f, 1.0f, 0.0f));
-    addParam(ParamWidget::create<LEDBezel>(mm2px(Vec(16.57, 38.164)), module, 2, 0.0f, 1.0f, 0.0f));
-    addParam(ParamWidget::create<LEDBezel>(mm2px(Vec(16.57, 48.165)), module, 3, 0.0f, 1.0f, 0.0f));
-    addParam(ParamWidget::create<LEDBezel>(mm2px(Vec(16.57, 58.164)), module, 4, 0.0f, 1.0f, 0.0f));
-    addParam(ParamWidget::create<LEDBezel>(mm2px(Vec(16.57, 68.165)), module, 5, 0.0f, 1.0f, 0.0f));
-    addParam(ParamWidget::create<LEDBezel>(mm2px(Vec(16.57, 78.164)), module, 6, 0.0f, 1.0f, 0.0f));
-    addParam(ParamWidget::create<LEDBezel>(mm2px(Vec(16.57, 88.164)), module, 7, 0.0f, 1.0f, 0.0f));
-    addParam(ParamWidget::create<LEDBezel>(mm2px(Vec(16.57, 98.165)), module, 8, 0.0f, 1.0f, 0.0f));
-    addParam(ParamWidget::create<LEDBezel>(mm2px(Vec(16.57, 108.166)), module, 9, 0.0f, 1.0f, 0.0f));
-
-    // The green light that displays when you click it
-    addChild(ModuleLightWidget::create<TriggerLight<GreenLight>>(mm2px(Vec(17.32, 18.915)), module, 0));
-    addChild(ModuleLightWidget::create<TriggerLight<GreenLight>>(mm2px(Vec(17.32, 28.916)), module, 1));
-    addChild(ModuleLightWidget::create<TriggerLight<GreenLight>>(mm2px(Vec(17.32, 38.915)), module, 2));
-    addChild(ModuleLightWidget::create<TriggerLight<GreenLight>>(mm2px(Vec(17.32, 48.915)), module, 3));
-    addChild(ModuleLightWidget::create<TriggerLight<GreenLight>>(mm2px(Vec(17.32, 58.916)), module, 4));
-    addChild(ModuleLightWidget::create<TriggerLight<GreenLight>>(mm2px(Vec(17.32, 68.916)), module, 5));
-    addChild(ModuleLightWidget::create<TriggerLight<GreenLight>>(mm2px(Vec(17.32, 78.915)), module, 6));
-    addChild(ModuleLightWidget::create<TriggerLight<GreenLight>>(mm2px(Vec(17.32, 88.916)), module, 7));
-    addChild(ModuleLightWidget::create<TriggerLight<GreenLight>>(mm2px(Vec(17.32, 98.915)), module, 8));
-    addChild(ModuleLightWidget::create<TriggerLight<GreenLight>>(mm2px(Vec(17.32, 108.915)), module, 9));
-
-    // Output ports
-    addOutput(Port::create<PJ301MPort>(Vec(86.393, 50.397), Port::OUTPUT, module, 0));
-    addOutput(Port::create<PJ301MPort>(Vec(86.393, 88.842), Port::OUTPUT, module, 1));
-    addOutput(Port::create<PJ301MPort>(Vec(86.393, 127.283), Port::OUTPUT, module, 2));
-    addOutput(Port::create<PJ301MPort>(Vec(86.393, 165.728), Port::OUTPUT, module, 3));
-    addOutput(Port::create<PJ301MPort>(Vec(86.393, 204.173), Port::OUTPUT, module, 4));
-    addOutput(Port::create<PJ301MPort>(Vec(86.393, 242.614), Port::OUTPUT, module, 5));
-    addOutput(Port::create<PJ301MPort>(Vec(86.393, 281.059), Port::OUTPUT, module, 6));
-    addOutput(Port::create<PJ301MPort>(Vec(86.393, 319.504), Port::OUTPUT, module, 7));
+    for (int i = 0; i < NUM_CHANNELS; i++) {
+        // Handles on click stuff
+        float y_dist = 38.445;
+        addParam(ParamWidget::create<RoundBlackKnob>(Vec(9.507, 50.397 + y_dist * i), module, PushButton::KNOB_PARAM + i, -1.0f, 1.0f, 0.0f));
+        addParam(ParamWidget::create<LEDBezel>(Vec(50, 53 + y_dist * i), module, PushButton::LIGHT_PARAM + i, 0.0f, 1.0f, 0.0f));
+        addChild(ModuleLightWidget::create<TriggerLight<GreenLight>>(Vec(52, 55 + y_dist * i), module, i));
+        addOutput(Port::create<PJ301MPort>(Vec(86.393, 50.414 + y_dist * i), Port::OUTPUT, module, i));
+    }
 }
 
 
