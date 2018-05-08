6c9a0f0af4d1fccfdc0deecf2af82fd2f2b8b3de
Aaron Kofsky <a2aarontothe2@gmail.com>
2018-05-08 15:00:38 -0400

    Align everything appropriately, also relabel the period multipler

--- src/NoiseGenerator.cpp
+++ src/NoiseGenerator.cpp
@@ -35,12 +35,12 @@ void NoiseGenerator::step() {
     // We then multiply the knob and CV inputs by 10V to obtain a nicer distribution of values.
     // This way, clock length can range from 1 to 1000 instead of just 1 to 10.
     // PERIOD_MULTIPLIER will increase the range from 1 to 100,000
-    float f_clock_length = (inputs[PERIOD_CV].value + params[PERIOD_KNOB].value) * 10.0f;
-    f_clock_length *= params[PERIOD_MULTIPLIER].value ? 1000.0f : 10.0f;
+    float f_clock_length = (params[PERIOD_KNOB].value + inputs[PERIOD_CV].value) * 10.0f;
+    f_clock_length *= params[PERIOD_MULTIPLIER].value ? 1000.0f : 1.0f;
     
     int clock_length = max(static_cast<int>(f_clock_length), 1);
 
-    float volume = params[VOLUME_KNOB].value;
+    float volume = params[VOLUME_KNOB].value + inputs[VOLUME_CV].value;
     // Every clock_length frames, set the current output to a new sample.
     // TODO: don't use the set frame rate, actually do this via time.
     if (timer % clock_length == 0){
@@ -62,17 +62,22 @@ NoiseGeneratorWidget::NoiseGeneratorWidget(NoiseGenerator *module) : ModuleWidge
     // Mounting Screws
     addChild(Widget::create<ScrewSilver>(Vec(5, 0)));
     addChild(Widget::create<ScrewSilver>(Vec(5, 365)));
-    addChild(Widget::create<ScrewSilver>(Vec(50, 0)));
+    addChild(Widget::create<ScrewSilver>(Vec(70, 0)));
     addChild(Widget::create<ScrewSilver>(Vec(50, 365)));
 
-    addOutput(Port::create<PJ301MPort>(Vec(10, 260), Port::OUTPUT, module, 0));
+    addOutput(Port::create<PJ301MPort>(Vec(50, 260), Port::OUTPUT, module, 0));
 
-    int x_pos = 20;
-    int y_offset = 5;
-    addParam(ParamWidget::create<RoundBlackKnob>(Vec(x_pos, 50 + y_offset), module, NoiseGenerator::VOLUME_KNOB, 0.0f, 12.0f, 1.0f));
-    
-    addParam(ParamWidget::create<RoundBlackKnob>(Vec(x_pos, 100 + y_offset), module, NoiseGenerator::PERIOD_KNOB, 0.0f, 10.0f, 0.0f));
+    int knob_x = 15;
+    int volume_y = 55;
+    int period_y = 105;
+    int cv_x = 58;
+    addParam(ParamWidget::create<RoundBlackKnob>(Vec(knob_x, volume_y), module, NoiseGenerator::VOLUME_KNOB, 0.0f, 12.0f, 1.0f));
+    addInput(Port::create<PJ301MPort>(Vec(cv_x, volume_y), Port::INPUT, module, NoiseGenerator::VOLUME_CV));
+
+    addParam(ParamWidget::create<RoundBlackKnob>(Vec(knob_x, period_y), module, NoiseGenerator::PERIOD_KNOB, 0.0f, 10.0f, 0.0f));
+
     addParam(ParamWidget::create<CKSS>(Vec(30, 150), module, NoiseGenerator::PERIOD_MULTIPLIER, 0.0f, 1.0f, 0.0f));
+    addInput(Port::create<PJ301MPort>(Vec(cv_x, period_y), Port::INPUT, module, NoiseGenerator::PERIOD_CV));
 }
 
 Model *modelNoiseGenerator = Model::create<NoiseGenerator, NoiseGeneratorWidget>("MicroTools", "Noise Generator", "Noise Generator", NOISE_TAG);
