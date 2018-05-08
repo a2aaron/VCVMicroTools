39ca4e1761de6a8b7e0d03d642c99bcb1735de22
Aaron Kofsky <a2aarontothe2@gmail.com>
2018-05-08 11:28:54 -0400

    Improve knob tunings and rename "frequency" to "period"
    This commit now make clock_length get casted at the end of the
    calculations of its value instead of the start to allow for finer
    tuning of the knob.

--- src/NoiseGenerator.cpp
+++ src/NoiseGenerator.cpp
@@ -3,9 +3,9 @@
 
 struct NoiseGenerator : Module {
     enum ParamIds {
-        FREQUENCY_KNOB = 0,
+        PERIOD_KNOB = 0,
         VOLUME_KNOB = 1,
-        FREQUENCY_MULTIPLIER = 2,
+        PERIOD_MULTIPLIER = 2,
         NUM_PARAMS = 3,
     };
 
@@ -18,7 +18,9 @@ struct NoiseGenerator : Module {
     };
 
     enum InputIds {
-        NUM_INPUTS = 0,
+        PERIOD_CV = 0,
+        VOLUME_CV = 1,
+        NUM_INPUTS = 2,
     };
 
     NoiseGenerator() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {}
@@ -28,19 +30,18 @@ struct NoiseGenerator : Module {
 };
 
 void NoiseGenerator::step() {
-    int clock_length = static_cast<int>(params[FREQUENCY_KNOB].value);
-    bool frequency_multiplier = params[FREQUENCY_MULTIPLIER].value;
 
-    if (frequency_multiplier) {
-        clock_length *= 100;
-    }
-
-    if (clock_length < 0) {
-        clock_length = 1;
-    }
+    // The knob and CV inputs are from 0V to 10V. (note that negative CV inputs will still work)
+    // We then multiply the knob and CV inputs by 10V to obtain a nicer distribution of values.
+    // This way, clock length can range from 1 to 1000 instead of just 1 to 10.
+    // PERIOD_MULTIPLIER will increase the range from 1 to 100,000
+    float f_clock_length = (inputs[PERIOD_CV].value + params[PERIOD_KNOB].value) * 10.0f;
+    f_clock_length *= params[PERIOD_MULTIPLIER].value ? 1000.0f : 10.0f;
+    
+    int clock_length = max(static_cast<int>(f_clock_length), 1);
 
     float volume = params[VOLUME_KNOB].value;
-
+    printf("%d\n", clock_length);
     // Every clock_length frames, set the current output to a new sample.
     if (timer % clock_length == 0){
         float sample = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
@@ -66,8 +67,8 @@ NoiseGeneratorWidget::NoiseGeneratorWidget(NoiseGenerator *module) : ModuleWidge
 
     addParam(ParamWidget::create<RoundBlackKnob>(Vec(8, 50), module, NoiseGenerator::VOLUME_KNOB, 0.0f, 12.0f, 1.0f));
     
-    addParam(ParamWidget::create<RoundBlackKnob>(Vec(8, 100), module, NoiseGenerator::FREQUENCY_KNOB, 1.0f, 100.0f, 1.0f));
-    addParam(ParamWidget::create<CKSS>(Vec(15, 150), module, NoiseGenerator::FREQUENCY_MULTIPLIER, 0.0f, 1.0f, 0.0f));
+    addParam(ParamWidget::create<RoundBlackKnob>(Vec(8, 100), module, NoiseGenerator::PERIOD_KNOB, 0.0f, 10.0f, 0.0f));
+    addParam(ParamWidget::create<CKSS>(Vec(15, 150), module, NoiseGenerator::PERIOD_MULTIPLIER, 0.0f, 1.0f, 0.0f));
 }
 
 Model *modelNoiseGenerator = Model::create<NoiseGenerator, NoiseGeneratorWidget>("MicroTools", "Noise Generator", "Noise Generator", NOISE_TAG);
